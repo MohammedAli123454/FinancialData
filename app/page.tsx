b74e1a165d2db706027aa687f748225f53656913
MohammedAli123454 <azcsmohdali1@gmail.com>
2025-04-20 21:11:07 +0300

    dfdf

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
-import { AuthWrapper } from '@/components/AuthWrapper';
+'use client';
+
+import { useCurrentUser } from '@/app/hooks/useCurrentUser';
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const { user, isLoading } = useCurrentUser();
+  const router = useRouter();
+
+  if (isLoading) {
+    return <div className="p-4">Loading...</div>;
+  }
+
+  if (!user) {
+    return (
+      <div className="flex justify-end p-4">
+        <Button onClick={() => router.push('/signin')}>Sign In</Button>
+      </div>
+    );
+  }
+
   return (
-    <AuthWrapper>
-      <div className="p-6">Welcome to the Home Page (secured content)</div>
-    </AuthWrapper>
+    <div className="p-6">
+      Welcome to the Home Page (secured content)
+    </div>
   );
 }
+
