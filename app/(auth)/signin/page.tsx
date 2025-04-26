bc0c63cb9cd0d863f2ec29cd6c66c2b6b4ea0696
MohammedAli123454 <azcsmohdali1@gmail.com>
2025-04-26 22:07:14 +0300

    df

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -5,7 +5,6 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { Alert, AlertTitle, AlertDescription } from '@/components/ui/alert';
-// import { signinAction } from '@/app/actions/signin'
 import { signinAction } from '../../actions/signin'
 export default function SigninPage() {
   const [isPending, startTransition] = useTransition();
