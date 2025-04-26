ff4e989d7b1a353075548a15c3b278cab607c5a0
MohammedAli123454 <azcsmohdali1@gmail.com>
2025-04-26 22:14:12 +0300

    sdfs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+// next.config.js
+
+import { NextConfig } from "next";
+import { Configuration } from "webpack";
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config: Configuration) {
+    config.resolve!.plugins = config.resolve!.plugins || [];
+    config.resolve!.plugins.push(
+      new TsconfigPathsPlugin({ configFile: "./tsconfig.json" })
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
