74aa3fc3160a0d19c58b52ac7722678f0926e3c7
MohammedAli123454 <azcsmohdali1@gmail.com>
2025-04-27 21:20:54 +0300

    Switch to flat eslint.config.js with eslint-config-next

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+// eslint.config.js
+/** @type {import('eslint').ESLint.ConfigData} */
+const config = {
+    extends: ["next"],
+    rules: {
+      // your custom rules, e.g.:
+      // "@typescript-eslint/no-explicit-any": "warn",
+    },
+  };
+  
+  module.exports = config;
+  
